19729a1178a521771cec2f969dc3d69ed0eaced2
Daniel Bornkessel <github@bornkessel.com>
2016-05-04 11:26:30 +0200

    Use printf instead of echo
    See problems with echo:
    http://pubs.opengroup.org/onlinepubs/9699919799/utilities/echo.html#tag_20_37_16

--- spec.sh
+++ spec.sh
@@ -16,7 +16,7 @@ test ! -t || IS_TTY=true
 run_test() {
   local function=$1
 
-  echo "=== RUN ${function}"
+  printf "=== RUN ${function}\n"
   local start=$(date "+%s")
   test -n "${VERBOSE}" && ( set -x; ${function} ) 2>&1 | tee /tmp/${function}.testlog || ( set -x; ${function} ) &> /tmp/${function}.testlog
   result=$?
@@ -31,7 +31,7 @@ run_test() {
     printf -- "--- FAIL: %s (%.2fs)\n" ${function} $(( $(date "+%s") - ${start} ))
     cat /tmp/${function}.testlog | sed 's/^/	/g'
     printf "\terror code: %d\n" ${result}
-    echo "error occured in ${function}"
+    printf "error occured in ${function}\n"
   fi
 }
 
@@ -40,7 +40,7 @@ SKIP_TEST() {
 }
 
 assert_match() {
-  echo $1 | grep -E -m1 -o "$2" | head -n1 | grep -E "$2"
+  printf "$1" | grep -E -m1 -o "$2" | head -n1 | grep -E "$2"
   assert 0 $? "checking '$1' to match '$2'"
 }
 
@@ -62,13 +62,13 @@ assert() {
   then
     if [ -n "${description}" ]
     then
-      echo -e "${IS_TTY:+\033[1;38;40m}${description}${IS_TTY:+\033[m}"
+      printf "${IS_TTY:+\033[1;38;40m}${description}${IS_TTY:+\033[m}\n"
     fi
-    echo -e "error: in test ${current_test} ${IS_TTY:+\033[1;38;40}mexpected '${result}' to be '${expected}'${IS_TTY:+\033[m}"
+    printf "error: in test ${current_test} ${IS_TTY:+\033[1;38;40}mexpected '${result}' to be '${expected}'${IS_TTY:+\033[m}\n"
     exit 1
   else
     set +x
-    echo "######################################## PASSED TEST: ${description:-${result} == ${expected}} "
+    printf "######################################## PASSED TEST: ${description:-${result} == ${expected}} \n"
     set -x
   fi
 }
@@ -83,13 +83,13 @@ run_tests() {
   functions=$(grep -Eho "(^it_[a-zA-Z_]*|^before_all|^after_all)" $0 ${__FILES})
 
   # call setup function if present
-  function_list=$(echo "${functions}" | grep -o before_all)
+  function_list=$(printf "${functions}" | grep -o before_all)
 
   # add all functions starting with 'it_' to  the function list
-  function_list="${function_list} $(echo "${functions}" | grep "^it_" | grep -E "${TESTS:-.}")"
+  function_list="${function_list} $(printf "${functions}" | grep "^it_" | grep -E "${TESTS:-.}")"
 
   # call tear down function if present
-  function_list="${function_list} $(echo "${functions}" | grep -o after_all)"
+  function_list="${function_list} $(printf "${functions}" | grep -o after_all)"
 
   for f in ${function_list}
   do
